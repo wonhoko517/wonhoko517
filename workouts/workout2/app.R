990e0cf187d2e0d9717baf53db8a3f0a93e7ea10
wonhoko517 <56706934+wonhoko517@users.noreply.github.com>
2019-11-13 19:35:49 -0800

    Add files via upload

--- workouts/workout2/app.R
+++ workouts/workout2/app.R
@@ -4,96 +4,89 @@ library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
     titlePanel("Title of your app: workout02.shiny app"),
+    
+    fluidRow(
+        column(3, sliderInput(inputId = "num",
+                              label = "Initial Amount",
+                              min = 1,
+                              max = 10000,
+                              value = 1000)),
+        column(3, sliderInput(inputId = "num",
+                              label = "High Yield annual rate (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 2)),
+        column(3, sliderInput(inputId = "num",
+                              label = "High Yield volatility (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 0.1)),
+        column(3, sliderInput(inputId = "num",
+                              label = "Years",
+                              min = 0,
+                              max = 50,
+                              value = 10))
+    ),
+    fluidRow(
+        column(3, sliderInput(inputId = "num",
+                              label = "Annual Contribution",
+                              min = 0,
+                              max = 5000,
+                              value = 200)),
+        column(3, sliderInput(inputId = "num",
+                              label = "Fixed Income annual rate (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 5)),
+        column(3, sliderInput(inputId = "num",
+                              label = "Fixed Income volatility (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 4.5)),
+        column(3, numericInput(inputId = "num", 
+                               label = h3("Random seed"), 
+                               value = 12345))
+    ),
+    fluidRow(
+        column(3, sliderInput(inputId = "num",
+                              label = "Annual Growth Rate (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 3)),
+        column(3, sliderInput(inputId = "num",
+                              label = "US Equity annual rate (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 10)),
+        column(3, sliderInput(inputId = "num",
+                              label = "US Equity volatility Rate (in %)",
+                              min = 0,
+                              max = 20,
+                              value = 15)),
+        column(3, selectInput('facet',
+                              'Facet', 
+                              c(Yes = '.', No = '.'), 
+                              selected = "No"))
+    ),
 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(inputId = "num",
-                        label = "Initial Amount",
-                        min = 1,
-                        max = 10000,
-                        value = 1000),
-            sliderInput(inputId = "num",
-                        label = "High Yield annual rate (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 2),
-            sliderInput(inputId = "num",
-                        label = "High Yield volatility (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 0.1),
-            sliderInput(inputId = "num",
-                        label = "Years",
-                        min = 0,
-                        max = 50,
-                        value = 10),
-            sliderInput(inputId = "num",
-                        label = "Annual Contribution",
-                        min = 0,
-                        max = 5000,
-                        value = 200),
-            sliderInput(inputId = "num",
-                        label = "Fixed Income annual rate (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 5),
-            sliderInput(inputId = "num",
-                        label = "Fixed Income volatility (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 4.5),
-            sliderInput(inputId = "num",
-                        label = "Annual Growth Rate (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 3),
-            sliderInput(inputId = "num",
-                        label = "US Equity annual rate (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 10),
-            sliderInput(inputId = "num",
-                        label = "US Equity volatility Rate (in %)",
-                        min = 0,
-                        max = 20,
-                        value = 15),
-            numericInput(inputId = "num", 
-                         label = h3("Numeric input"), 
-                         value = 1),
-            selectInput('facet',
-                        'Facet', 
-                        c(Yes = '.', No = '.'), 
-                        selected = "No")
-        ),
 
-        # Show a plot of the generated distribution
+
+        # Show a line of the generated distribution
         mainPanel(
-           plotOutput("hist")
-        )
+           plotOutput("line")
     )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    set.seed(12345)
-    # mean = rate_bonds
-    # sd = vol_bonds
-    trace_0 <- rnorm(1, mean = 0.02, sd = 0.001)
-    trace_1 <- rnorm(1, mean = 0.05, sd = 0.045)
-    trace_2 <- rnorm(1, mean = 0.1, sd = 0.15)
-    x <- c(0:1)
-    data <- data.frame(x, trace_0, trace_1, trace_2)
-    p <- plot_ly(data, x = ~x)
-        add_trace(y = ~trace_0, name = 'trace 0',mode = 'lines')
-        add_trace(y = ~trace_1, name = 'trace 1', mode = 'lines')
-        add_trace(y = ~trace_2, name = 'trace 2', mode = 'lines')
-# if at least one facet column/row is specified, add it
-facets <- paste(input$facet_row, '~', input$facet_col)
-if (facets != '. ~ .') p <- p + facet_grid(facets)
-}
 
+# Define server logic required to draw a timeline
+server <- function(input, output) {
+    data <- reactive({
+        rnorm(input$num)
+    })
+    output$line <- renderPlot({
+        line(rnorm(input$num))
+    })
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
